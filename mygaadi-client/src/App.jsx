1362d37cbc93247ae320d8712734461827d60eb7
RajPakhurde <rajofficial4803@gmail.com>
2025-11-05 18:31:56 +0530

    added basic route setup

--- mygaadi-client/src/App.jsx
+++ mygaadi-client/src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import UserLayout from "./Components/UserLayout";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+
 function App() {
-  return <></>;
+  return (
+  <BrowserRouter>
+    <div>
+      <Routes>
+        <Route index element=<UserLayout/> >
+
+        </Route>
+      </Routes>
+
+    </div>
+  </BrowserRouter>
+  )
 }
 
 export default App;
